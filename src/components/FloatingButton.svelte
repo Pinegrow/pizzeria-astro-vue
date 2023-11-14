2adac58b9c070b7898b9d6ce322764fd0b811f0d
Akayy <techakayy@gmail.com>
2023-11-14 13:38:48 +1100

    Updated to svelte color

--- src/components/FloatingButton.svelte
+++ src/components/FloatingButton.svelte
@@ -1,5 +1,5 @@
 <button
-  class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium text-base gap-x-2 px-4 py-3 text-primary-500 dark:text-primary-400 bg-primary-50 hover:bg-primary-100 dark:bg-primary-950 dark:hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center !rounded-3xl bottom-12 fixed right-2 z-20"
+  class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium text-base gap-x-2 px-4 py-3 text-[#c45d3a] dark:text-[#d37a5b] bg-[#f7eeec] hover:bg-[#f1d0c6] dark:bg-[#743722] dark:hover:bg-[#743722] focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-[#c45d3a] dark:focus-visible:ring-[#c45d3a] inline-flex items-center !rounded-3xl bottom-12 fixed right-2 z-20"
   type="button"
   ><span
     class="i-material-symbols-shopping-basket flex-shrink-0 h-6 w-6"
